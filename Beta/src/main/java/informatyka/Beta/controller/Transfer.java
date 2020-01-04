3480a818b25f2432d8e91fc3b8ac0d8f10903e46
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2020-01-04 19:21:42 +0100

    start pracy na formularem przelewów

--- Beta/src/main/java/informatyka/Beta/controller/Transfer.java
+++ Beta/src/main/java/informatyka/Beta/controller/Transfer.java
@@ -1,14 +1,53 @@
 package informatyka.Beta.controller;
 
+import informatyka.Beta.model.User;
+import informatyka.Beta.repository.UserRepository;
+import informatyka.Beta.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class Transfer {
 
-    @RequestMapping("/transfer")
-    public String wireTransfer(){
+    @Autowired
+    private UserService userService;
 
-        return "transfer";
+    @RequestMapping(value = "/user/transfer.html", method = RequestMethod.GET)
+    public ModelAndView wireTransfer(){
+
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", "Witaj " + user.getName() + " " + user.getPersonId() + " (" + user.getEmail() + ")");
+        modelAndView.addObject("accountNumber", "Numer konta: " + user.getAccountNumber());
+        modelAndView.addObject("saldo", "Stan Twojego konta: " + user.getSaldo() + "zł");
+        modelAndView.addObject("adminMessage","Twoje dane dostępne są tylko po zalogowaniu na Twoje konto :)");
+        modelAndView.setViewName("user/transfer");
+        return modelAndView;
+    }
+
+    @PostMapping("/transferdo")
+    public ModelAndView doTransfer(HttpServletRequest request){
+
+        ModelAndView modelAndView = new ModelAndView();
+        String a = request.getParameter("accountnumber");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", "Witaj " + user.getName() + " " + user.getPersonId() + " (" + user.getEmail() + ")");
+        modelAndView.addObject("accountNumber", "Numer konta: " + user.getAccountNumber());
+        modelAndView.addObject("saldo", "Stan Twojego konta: " + user.getSaldo() + "zł");
+        modelAndView.addObject("adminMessage","Twoje dane dostępne są tylko po zalogowaniu na Twoje konto :)");
+        modelAndView.addObject(a);
+        modelAndView.setViewName("user/transfer");
+        return modelAndView;
+
     }
 }
